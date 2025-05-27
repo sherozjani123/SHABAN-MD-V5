8c73df3cff22b2df76d4d76819a56280cbf7eca8
sherozjani123 <sherozshoukatch@gmail.com>
2025-05-28 00:14:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~7BkUAKYA#Y655chAPbD6DRqNU8Btm8PivaVNHyctHqeA38OXTIoc",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
